59645d5c8cce9e19b41e5785dc2d88277cd47741
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-04 12:21:36 -0700

    Initial setup for express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json());
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
