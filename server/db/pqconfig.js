599e6de736384276118d93012dfcbdaaf4a1f338
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-04 12:22:04 -0700

    Intialize postgres configuration model

---
+++ server/db/pqconfig.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg').Pool;
+const pool = new Pool ({
+    user: 'postgres',
+    password: 'postgres',
+    databse: 'bugsDB',
+    port: 5432
+})
+
+
+
+module.exports = {
+    pool
+}
