6e6c69519c68efeb1399fd24b83bf46d1efb729d
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-04 15:37:38 -0700

    change server port to 3000

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8080;
+const port = 3000;
 const db = require('./db/queries.js');
+const cors = require('cors');
+
 
 app.use(bodyParser.json());
-
+app.use(cors());
 
 app.get('/', db.getAll)
 app.post('/', db.createBug)
