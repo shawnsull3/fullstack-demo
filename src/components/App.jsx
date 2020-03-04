74d2b28244cb37544292104160cadbdd5e144025
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-04 15:38:36 -0700

    render bug data from server upon retrevial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     super();
     this.state = {
       filter: 'None',
-      bugs: exampleData,
+      bugs: null,
     };
     this.filterHandler = this.filterHandler.bind(this);
   }
@@ -19,38 +19,49 @@ class App extends React.Component {
     this.setState({ filter });
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/')
+      .then(data => data.json())
+      .then(data => {
+        console.log('data HERE:', data);
+        this.setState({ bugs: data})
+      })
+  }
+
   
   render() {
     return (
-      <table>
-        <Nav
-          filterHandler={this.filterHandler}
-        />
-        {this.state.bugs.map((bug) => (
-          this.state.filter === 'None' ? (
-            <BugTile
-              bugName={bug.bugName}
-              bugDescription={bug.bugDescription}
-              reportedBy={bug.reportedBy}
-              createdDate={bug.createdDate}
-              assignedTo={bug.assignedTo}
-              threatLevel={bug.threatLevel}
-              key={bug.bugName}
-            />
-          ) : bug.threatLevel === this.state.filter ? (
-            <BugTile
-              bugName={bug.bugName}
-              bugDescription={bug.bugDescription}
-              reportedBy={bug.reportedBy}
-              createdDate={bug.createdDate}
-              assignedTo={bug.assignedTo}
-              threatLevel={bug.threatLevel}
-              key={bug.bugName}
-            />
-          ) : null
-        ))}
-
-      </table>
+      <div>
+        <button>Add Bug</button>
+        <table>
+          <Nav filterHandler={this.filterHandler}/>
+          {this.state.bugs ? 
+            this.state.bugs.map((bug) => (
+              this.state.filter === 'None' ? (
+                <BugTile
+                  bugName={bug.bugName}
+                  bugDescription={bug.bugDescription}
+                  reportedBy={bug.reportedBy}
+                  createdDate={bug.createdDate}
+                  assignedTo={bug.assignedTo}
+                  threatLevel={bug.threatLevel}
+                  key={bug.bugName}
+                />
+              ) : bug.threatLevel === this.state.filter ? (
+                <BugTile
+                  bugName={bug.bugName}
+                  bugDescription={bug.bugDescription}
+                  reportedBy={bug.reportedBy}
+                  createdDate={bug.createdDate}
+                  assignedTo={bug.assignedTo}
+                  threatLevel={bug.threatLevel}
+                  key={bug.bugName}
+                />
+              ) : null
+            )) : null
+          }
+        </table>
+      </div>
     );
   }
 }
